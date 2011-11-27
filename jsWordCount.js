813547b98294b3d52b3d0365e557274a742e7705
Rich Boakes <rich.boakes@port.ac.uk>
2011-11-27 19:15:18 +0000

    First Commit
    Basic functionality working.

---
+++ jsWordCount.js
@@ -0,0 +1,45 @@
+/*
+Free Software: see license.txt Copyright (c) 2011 Rich Boakes
+*/
+var jsWordCount = (function () {
+	"use strict";
+	var
+		countWordsInText = function (str) {
+			var localCount = 0,
+				words = str.trim().split(" "),
+				i;
+			for (i = 0; i < words.length; i++) {
+				if (words[i].trim().length > 0) {
+					localCount++;
+				}
+			}
+			return localCount;
+		},
+		countWordsInElement = function (node) {
+			var localCount = 0, i;
+			for (i = 0; i < node.childNodes.length; i++) {
+				switch (node.childNodes[i].nodeType) {
+				case 1:
+					localCount += countWordsInElement(node.childNodes[i]);
+					node.setAttribute("data-jsWordCount", localCount);
+					break;
+				case 3:
+					localCount += countWordsInText(node.childNodes[i].textContent);
+					break;
+				}
+			}
+			return localCount;
+		},
+		annotate = function () {
+			var topLevelCountables = document.querySelectorAll("article"),
+				i;
+			for (i = 0; i < topLevelCountables.length; i++) {
+				countWordsInElement(topLevelCountables[i]);
+			}
+		};
+	return {
+		annotate: annotate
+	};
+}());
+
+window.addEventListener("load", jsWordCount.annotate);
---
+++ readme.md
@@ -0,0 +1,19 @@
+# jsWordCount
+This code counts the words within in HTML documents providing summaries for paragraphs, sections, articles and the body.  Summaries are added to elements using HTML5 data attributes under the name `data-jsWordCount`.
+
+## Use
+
+1. Add jsWordCount.js to any HTML5 document.
+2. Add jsWordCount.css to the docmument.
+3. Load the document.
+3. Observe that word counts are added to elements.
+
+The sample cascading style sheet uses this data-jsWordCount value to augment on-screen content using the :before pseudo selector.
+
+## License
+Free Software: see [license.txt][license.txt]
+Copyright (c) 2011 Rich Boakes http://boakes.org
+
+[license]: jsWordCount/license.txt "MIT License (GPL v3 compatible)
+[modal]: http://example.com/  "Modal Dialogue explained on Wikipedia"
+  
